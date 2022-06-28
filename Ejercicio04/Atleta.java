cdeeb1e0e1b824dddd2ee022eb2a79ce61ff6cd1
Emmanuel <105991940+Emmlg@users.noreply.github.com>
2022-06-27 22:21:12 -0500

    Add files via upload

---
+++ Ejercicio04/Atleta.java
@@ -0,0 +1,35 @@
+/*
+Construir un programa para una competencia de atletismo, el programa debe gestionar una serie de atletas caracterizados
+por su número de atleta, nombre y tiempo de carrera, al final el programa debe mostrar los datos del atleta ganador de la carrera.
+ */
+package Ejercicio04;
+
+/**
+ *
+ * @author Emmanuel lopez 27/junio/2022
+ */
+public class Atleta {
+    
+    //atributo
+    private String nombre;
+    private int numeroDeAtleta;
+    private float tiempoDeCarrera;
+    //constructor
+    public Atleta(String nombre,int numeroDeAtleta,float tiempoDeCarrera){
+    this.nombre=nombre;
+    this.numeroDeAtleta=numeroDeAtleta;
+    this.tiempoDeCarrera=tiempoDeCarrera;
+    }
+    
+    public float getTiempo(){
+    return tiempoDeCarrera;
+    }
+    public String MostrarDatos(){
+    return "\n\tNombre: "+nombre
+            + "\n\tNumero de Atleta: "+numeroDeAtleta
+            + "\n\tTiempo de la carrera: "+tiempoDeCarrera;
+    }
+    
+    
+    
+}
