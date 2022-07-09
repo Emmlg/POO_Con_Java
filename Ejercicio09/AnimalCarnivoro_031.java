a89419d237137a15e2cf088b6795601c9b439429
Emmanuel <105991940+Emmlg@users.noreply.github.com>
2022-07-09 14:37:49 -0500

    Add files via upload

---
+++ Ejercicio09/AnimalCarnivoro_031.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio09;
+
+/**
+ *
+ * @author Emmanuel lopez 9/julio/2022
+ */
+public class AnimalCarnivoro_031 {
+    
+    public void alimentarse(){
+        System.out.println("Este animal se alimenta por carne");
+        
+    }
+    
+}
---
+++ Ejercicio09/Animal_03.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio09;
+
+/**
+ *
+ * @author Emmanuel lopez 9/julio/2022
+ */
+public abstract class Animal_03 extends SerVivo_01{
+    
+}
