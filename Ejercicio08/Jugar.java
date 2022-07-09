6d9c4d8d764246da254dcaf4fcc39a92f91fb8a8
Emmanuel <105991940+Emmlg@users.noreply.github.com>
2022-07-09 12:37:41 -0500

    Add files via upload

---
+++ Ejercicio08/Jugar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio08;
+
+/**
+ *
+ * @author Emmanuel lopez 9/julio/2022
+ */
+public class Jugar {
+    
+    
+ public void golpearBalon(){
+ 
+     System.out.println("juego con el balon");
+ }
+    
+    
+}
