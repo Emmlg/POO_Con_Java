f4330aec3e473604b97d6d8afd83bb02c79d5889
Emmanuel <105991940+Emmlg@users.noreply.github.com>
2022-07-09 22:45:22 -0500

    Add files via upload

--- Ejercicio11/Main_0.java
+++ Ejercicio11/Main_0.java
@@ -1,55 +1,104 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Ejercicio 9: Hacer un programa para calcular el área de Polígonos (Triángulos y Rectángulos) 
+el programa debe ser capaz de almacenar en un arreglo N triángulos y rectángulos, 
+y al final mostrar el área y los datos de cada uno. Para ello se tendrá lo siguiente: 
+    - Una super clase llamada Polígono.
+    - Una sub clase llamada Rectángulo.
+    - Una sub clase llamada Triángulo.
  */
 package Ejercicio11;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
- *
- * @author Emmanuel lopez 9/julio/2022
+ * @author Emmanuel Lopez 9/julio/2022
  */
 public class Main_0 {
-    public static void main(String[] args) {
-      
-     SpiderMan_01 spiders[]=new SpiderMan_01[4];
+    
+  static ArrayList<Poligono_01> poligonos =new ArrayList<Poligono_01>();   
+  static Scanner tecla=new Scanner(System.in);
+    
+
+    
+    
+public static void main(String[] args) {
+        
+        llenarPoligono();
+        mostrarResultado();
+   
+    }// fin del MAIn
+
+
+
+    public static void llenarPoligono(){
+        
+        byte opcion;
+        char respuesta;
+        do { 
+            
+            do{
+            System.out.println("Digite el poligono que necesite: ");
+            System.out.println("\n1.- Triangulo \n2.- Rectangulo");
+            System.out.println("opcion: ");
+            opcion=tecla.nextByte();
+            }while(opcion <1 || opcion>2);
+            
+            switch(opcion){
+                case 1: llenarTriangulo();
+                    break;
+                case 2: llenarRectangulo();
+
+                    break;
+            }
+            
+            System.out.print("Desea ingresar otro poligono: S/N ");
+            respuesta=tecla.next().charAt(0);
+            
+        } while (respuesta=='s' || respuesta=='S');
+    
+    }
+    
+    public static void llenarTriangulo(){
+     double lado1,lado2,lado3;
      
-     spiders[0]=new SpiderMilesMorales_02("Miles morales","Tierra-1610","Kangaroo","Aaron Davis");
-     spiders[1]=new SpiderWomen_03("Gwen Stacy", "Tierra-65","King Pin", "Ghost spider o White Widow");
-     spiders[2]=new SpiderPeniParker_04(" Peni Parker", "Tierra-14512", "sin Enemigos", "Japonesa");
-     spiders[3]=new SpiderHam_05("Peter Benjamin Porker", "Tierra-8311", " Rana-Toro", "cerdo mutado");
+        System.out.println("Digite el lado 1:");
+        lado1=tecla.nextDouble();
+        System.out.println("Digite el lado 2:");
+        lado2=tecla.nextDouble();
+        System.out.println("Digite el lado 3:");
+        lado3=tecla.nextDouble();
         
-        for(SpiderMan_01 spiderMan_01:spiders){
-            System.out.println(spiderMan_01.mostrarDatos());
-            System.out.println("");
-        }
-        
-        
-        // son dos formas de instanciar los objetos
-        
-     
-        /*
-        
-    //    SpiderMan_01 spiderman=new SpiderMan_01("piter Parker", "tierra", "varios villanos");
-        SpiderMan_01 MilesMorales=new SpiderMilesMorales_02("Miles morales","Tierra-1610","Kangaroo","Aaron Davis");
-        SpiderMan_01 spiderWomen=new SpiderWomen_03("Gwen Stacy", "Tierra-65","King Pin", "Ghost spider o White Widow");
-        SpiderMan_01 peniParker=new SpiderPeniParker_04(" Peni Parker", "Tierra-14512", "sin Enemigos", "Japonesa");
-        SpiderMan_01 spiderHam=new SpiderHam_05("Peter Benjamin Porker", "Tierra-8311", " Rana-Toro", "cerdo mutado");
-        
-        System.out.println(//spiderman.mostrarDatos()+
-        "\n"+ MilesMorales.mostrarDatos()+
-        "\n"+spiderWomen.mostrarDatos()+
-        "\n"+ peniParker.mostrarDatos()+
-        "\n"+spiderHam.mostrarDatos());
-     
-        */
+        Triangulo_03 triangulo=new Triangulo_03(lado1, lado2, lado3);
+        poligonos.add(triangulo);//guardamos el objeto triangulo con sus datos dentro de nuestro arreglo
+    
+
+    }
+    
        
-        
-      
-        
-        
-        
-        
-        
-    }    
+    public static void llenarRectangulo(){
+    double lado1,lado2;
+        System.out.print("\nDigita el lado 1: ");
+        lado1=tecla.nextDouble();
+        System.out.print("\nDigita el lado 2: ");
+        lado2=tecla.nextDouble();
+    
+        Rectangulo_02 rectangulo=new Rectangulo_02(lado1, lado2);
+        poligonos.add(rectangulo);
+    
+    }
+    
+        public static void mostrarResultado(){
+    
+    for(Poligono_01 poli:poligonos){
+        System.out.println(poli.toString());
+        System.out.println("\nEl area es: "+poli.area());
+        System.out.println(" ");
+    }
+
+    }
+
+
+
+
 }
